7426a63ad875f8e5d351a144c1ef803155172aed
dropout <dropout@dpt.wtf>
2021-04-10 00:59:25 +0200

    formatting help

--- src/main/java/wtf/dpt/beelzebot/helpers/BotHelper.java
+++ src/main/java/wtf/dpt/beelzebot/helpers/BotHelper.java
@@ -14,11 +14,11 @@ public class BotHelper {
     public static final String LINK_ISSUES = "https://github.com/dropout1692/BeelzeBot/issues";
 
     private final List<String> commands = List.of(
-            "!help          - shows currently available commands",
-            "!report        - links to the issue reporting page",
-            "!about         - links to GitHub/master",
-            "!prievan       - shows last " + PrievanService.LIST_LIMIT + " voice channel events",
-            "!chuck         - posts a savage Chuck Norris joke",
-            "!9gag <link>   - extracts a meme from a 9gag link; if it's a video it gets embedded"
+            "!help              - shows currently available commands",
+            "!report            - links to the issue reporting page",
+            "!about             - links to GitHub/master",
+            "!prievan           - shows last " + PrievanService.LIST_LIMIT + " voice channel events",
+            "!chuck             - posts a savage Chuck Norris joke",
+            "!9gag <link>       - extracts a meme from a 9gag link; if it's a video it gets embedded"
     );
 }
