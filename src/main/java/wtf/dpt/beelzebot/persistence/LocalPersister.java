ceeedd7d795055678bce86899209bfb13a05cb16
Dropout <michalmarusinec@gmail.com>
2021-06-05 16:59:00 +0200

    save return type & load impl

--- src/main/java/wtf/dpt/beelzebot/persistence/LocalPersister.java
+++ src/main/java/wtf/dpt/beelzebot/persistence/LocalPersister.java
@@ -2,7 +2,7 @@ package wtf.dpt.beelzebot.persistence;
 
 public abstract class LocalPersister<T extends Object> {
 
-    public abstract void save(T savedObject);
+    public abstract boolean save(T savedObject);
 
     public abstract T load();
 }
--- src/main/java/wtf/dpt/beelzebot/persistence/PrievanLocalPersister.java
+++ src/main/java/wtf/dpt/beelzebot/persistence/PrievanLocalPersister.java
@@ -1,12 +1,15 @@
 package wtf.dpt.beelzebot.persistence;
 
 import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import wtf.dpt.beelzebot.model.PrievanEventDTO;
 
 import javax.annotation.PostConstruct;
 import java.io.*;
+import java.lang.reflect.Type;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.List;
@@ -18,14 +21,14 @@ public class PrievanLocalPersister extends LocalPersister<List<PrievanEventDTO>>
     public final static String FILE_NAME = "prievan.bpf";
 
     @Override
-    public void save(List<PrievanEventDTO> savedObject) {
+    public boolean save(List<PrievanEventDTO> savedObject) {
 
         String originalFilePath = String.format("%s%s", FILE_PATH, FILE_NAME);
         String tempFilePath = String.format("%s.temp", originalFilePath);
         File originalFile = new File(originalFilePath);
         File tempFile = new File(tempFilePath);
 
-        try (FileWriter fw = new FileWriter(tempFile)){
+        try (FileWriter fw = new FileWriter(tempFile)) {
 
             Gson gson = new Gson();
             String jsonString = gson.toJson(savedObject);
@@ -35,11 +38,33 @@ public class PrievanLocalPersister extends LocalPersister<List<PrievanEventDTO>>
 
         } catch (IOException ignored) {
             //todo: handle
+            return false;
         }
+
+        return true;
     }
 
     @Override
     public List<PrievanEventDTO> load() {
-        return new ArrayList<>();
+
+        String filePath = String.format("%s%s", FILE_PATH, FILE_NAME);
+        File file = new File(filePath);
+
+        String jsonString = "{}";
+        try (FileInputStream fis = new FileInputStream(file)) {
+
+            byte[] bytes = new byte[(int) file.length()];
+            fis.read(bytes);
+            jsonString = new String(bytes, StandardCharsets.UTF_8);
+
+        } catch (IOException e) {
+            //todo: handle
+        }
+
+        Type listOfPrievanEventDTOs = new TypeToken<ArrayList<PrievanEventDTO>>() {
+        }.getType();
+        Gson gson = new Gson();
+
+        return gson.fromJson(jsonString, listOfPrievanEventDTOs);
     }
 }
